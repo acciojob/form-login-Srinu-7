1272994db93274bc6b0b4c98d49ce9bb1bff1877
Srinu-7 <srinumaka9908@gmail.com>
2024-08-22 08:53:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
-    //Write your code here
+    // Prevent the form from actually submitting and refreshing the page
+    event.preventDefault();
 
+    // Access the first name and last name input elements using their name attributes
+    const firstName = document.querySelector('input[name="fname"]').value;
+    const lastName = document.querySelector('input[name="lname"]').value;
+
+    // Combine the first name and last name and display them in an alert
+    alert(firstName + ' ' + lastName);
 }
+
